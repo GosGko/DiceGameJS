92a3a8564f6f7f27600d7c5cf2cf1e0dbde2dbd4
GosGko <bosspetkov@abv.bg>
2023-04-30 11:56:08 +0300

    haahaha

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var list = [images/dice6.png, images/dice5.png, images/dice4.png, images/dice3.png, images/dice2.png, images/dice1.png];
 var random = Math.random();
 random = (random * 6) + 1;
 random = Math.floor(random) ;
+var image = document.querySelector(".diceImg");
