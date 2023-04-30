1c2c50f460ebda1871105c89d7baa6704a9da1d2
GosGko <bosspetkov@abv.bg>
2023-04-30 12:17:52 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var list = [images/dice6.png, images/dice5.png, images/dice4.png, images/dice3.png, images/dice2.png, images/dice1.png];
-var random = Math.random();
-random = (random * 6) + 1;
-random = Math.floor(random) ;
+var list = ["images/dice6.png", "images/dice5.png", "images/dice4.png", "images/dice3.png", "images/dice2.png", "images/dice1.png"];
+var random1 = Math.floor(Math.random() * list.length);
+var random2 = Math.floor(Math.random() * list.length);
 var image = document.querySelector(".diceImg");
+var image2 = document.querySelector(".diceImgg");
+var dice = image.src = list[random1];
+var dice2 = image2.src = list[random2];
