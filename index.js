999b5e22fde5e2a0606ac0da85736517e7688dea
GosGko <bosspetkov@abv.bg>
2023-04-30 14:16:12 +0300

    dw

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ var image = document.querySelector(".diceImg");
 var image2 = document.querySelector(".diceImgg");
 var dice = image.src = list[random1];
 var dice2 = image2.src = list[random2];
+
+if (random1 > random2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 win";
+}
+else if (random1 < random2) {
+    document.querySelector("h1").innerHTML = "Player 2 win🚩";
+}
+else {
+    document.querySelector("h1").innerHTML = "🥱Draw🥱";
+} 
